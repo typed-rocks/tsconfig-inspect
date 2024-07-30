e95f808c68cf2a6829252b7dac2097c1687d1980
Christian Woerz <4927839+chwoerz@users.noreply.github.com>
2024-07-30 21:04:00 +0200

    some new code

--- README.md
+++ README.md
@@ -2,13 +2,90 @@
 
 `tsconfig-inspect` is a CLI tool designed to provide an enhanced view of the TypeScript configuration options, offering
 deeper insights than the standard `--showConfig` parameter. It allows users to inspect the tsconfig file more
-thoroughly, including defaults and resolved files.
+thoroughly, including defaults and more complex dependencies.
 
 ## Features
 
 - **Custom Path Specification**: Specify the path to your tsconfig file.
 - **View Default Options**: Choose whether to display unchanged default options.
 - **Resolve Included Files**: Option to display files resolved by the tsconfig.
+
+## Example
+
+### Input
+
+```json
+{
+  "compilerOptions": {
+    "target": "NodeNext"
+  }
+}
+```
+
+### Output difference
+
+<table>
+<thead>
+<td><code>tsc --showConfig</code></td><td><code>tsconfig-inspect</code></td></thead>
+<tr>
+<td>
+
+```json
+{
+  "module": "nodenext",
+  "target": "esnext",
+  "moduleResolution": "nodenext",
+  "moduleDetection": "force",
+  "esModuleInterop": true,
+  "allowSyntheticDefaultImports": true,
+  "useDefineForClassFields": true
+}
+```
+
+</td>
+<td>
+
+```json
+{
+  "incremental": false,
+  "target": "esnext",
+  "useDefineForClassFields": true,
+  "moduleDetection": "force",
+  "module": "nodenext",
+  "moduleResolution": "nodenext",
+  "resolvePackageJsonExports": true,
+  "resolvePackageJsonImports": true,
+  "resolveJsonModule": false,
+  "allowJs": false,
+  "declaration": false,
+  "declarationMap": false,
+  "newLine": "lf",
+  "preserveConstEnums": false,
+  "isolatedModules": false,
+  "allowSyntheticDefaultImports": true,
+  "esModuleInterop": true,
+  "forceConsistentCasingInFileNames": true,
+  "noImplicitAny": false,
+  "strictNullChecks": false,
+  "strictFunctionTypes": false,
+  "strictBindCallApply": false,
+  "strictPropertyInitialization": false,
+  "noImplicitThis": false,
+  "useUnknownInCatchVariables": false,
+  "alwaysStrict": false,
+  "charset": "utf8",
+  "importsNotUsedAsValues": "remove",
+  "jsxFactory": "React.createElement",
+  "jsxFragmentFactory": "React.Fragment",
+  "jsxImportSource": "react",
+  "reactNamespace": "React",
+  "pretty": true
+}
+```
+
+</td>
+</tr>
+</table>
 
 ## Installation
 
@@ -36,6 +113,12 @@ tsconfig-inspect [options]
 - `--showFiles <value>`: Enable or disable showing resolved included files (`true/false`). Default is `true`.
 
 ### Examples
+
+**Inspect the `tsconfig.json` in the current path**
+
+```bash
+tsconfig-inspect
+```
 
 **Inspect a Specific tsconfig File**
 
--- src/cli.ts
+++ src/cli.ts
@@ -1,40 +1,59 @@
 #!/usr/bin/env node
 
-import {CliArgs, inspect, TSConfig} from "./api";
-import path from 'path';
-import fs from 'fs';
-import chalk from 'chalk';
+import { CliArgs, inspect, TSConfig } from "./api";
+import path from "path";
+import fs from "fs";
+import chalk from "chalk";
 
-import {Command} from "commander";
-import {CompilerOptions, Diagnostic} from "typescript";
+import { Command } from "commander";
+import { CompilerOptions, Diagnostic } from "typescript";
 
 const program = new Command();
 
 program
   .version("1.0.1")
   .description("tsconfig-inspect")
-  .option("--path <value>", "The path to the tsconfig file to inspect", './tsconfig.json')
-  .option("--defaults <value>", "enable or disable showing not changed defaults (true/false)", true)
-  .option("--showFiles <value>", "enable or disable showing to resolve included files (true/false)", true)
+  .option(
+    "--path <value>",
+    "The path to the tsconfig file to inspect",
+    "./tsconfig.json",
+  )
+  .option(
+    "--defaults <value>",
+    "enable or disable showing not changed defaults (true/false)",
+    true,
+  )
+  .option(
+    "--showFiles <value>",
+    "enable or disable showing to resolve included files (true/false)",
+    true,
+  )
   .parse(process.argv);
 
 const options = program.opts();
 
-
 function loadArgs(): CliArgs | undefined {
   const tsConfigPath = options.path;
   const absolute = path.resolve(tsConfigPath);
-  if(!fs.existsSync(absolute)) {
+  if (!fs.existsSync(absolute)) {
     console.error(`tsconfig file at ${absolute} does not exist.`);
     return undefined;
   }
-  const withDefaults = options['defaults'] !== true ? options['defaults'] === 'true' : true;
-  const showFiles = options['showFiles'] !== true ? options['showFiles'] === 'true' : true;
-  return {tsConfigPath: path.resolve(tsConfigPath), withDefaults: withDefaults, showFiles: showFiles};
+  const withDefaults =
+    options["defaults"] !== true ? options["defaults"] === "true" : true;
+  const showFiles =
+    options["showFiles"] !== true ? options["showFiles"] === "true" : true;
+  return {
+    tsConfigPath: path.resolve(tsConfigPath),
+    withDefaults: withDefaults,
+    showFiles: showFiles,
+  };
 }
 
-function isDiagnostics(input: ReturnType<typeof inspect>): input is {diagnostics: Diagnostic} {
-  return !!(input as {diagnostics: Diagnostic}).diagnostics;
+function isDiagnostics(
+  input: ReturnType<typeof inspect>,
+): input is { diagnostics: Diagnostic } {
+  return !!(input as { diagnostics: Diagnostic }).diagnostics;
 }
 
 const config = loadArgs();
@@ -47,24 +66,32 @@ if (isDiagnostics(result)) {
   process.exit(1);
 }
 
- const {generated, realCompilerConfig} = result as {
-   generated: TSConfig,
-   realCompilerConfig: CompilerOptions
- };
-  const keys: (keyof CompilerOptions)[] = Object.keys(realCompilerConfig);
-  const allKeys = Object.keys(generated.compilerOptions);
-  const newKeys = allKeys.filter(k => !keys.includes(k));
-  const generatedStringedLine = JSON.stringify(generated, null, 2).split('\n');
+const { generated, realCompilerConfig, onlyGenerated } = result as {
+  generated: TSConfig;
+  realCompilerConfig: CompilerOptions;
+  onlyGenerated: CompilerOptions;
+};
+const keys: (keyof CompilerOptions)[] = Object.keys(realCompilerConfig);
+const allKeys = Object.keys(generated.compilerOptions);
+const newKeys = allKeys.filter((k) => !keys.includes(k));
+const generatedKeys = Object.keys(onlyGenerated);
+const generatedStringedLine = JSON.stringify(generated, null, 2).split("\n");
 
-
-  const output = generatedStringedLine.map(line => {
-    const isNewKey = newKeys.some(newKey => line.trim().startsWith(`"${newKey}":`));
-    if(isNewKey) {
+const output = generatedStringedLine
+  .map((line) => {
+    const isNewKey = newKeys.some((newKey) =>
+      line.trim().startsWith(`"${newKey}":`),
+    );
+    const isGenerated = generatedKeys.some((newKey) =>
+      line.trim().startsWith(`"${newKey}":`),
+    );
+    if (isGenerated) {
+      //return chalk.blue(line);
+    }
+    if (isNewKey) {
       return chalk.green(line);
     }
     return line;
-  }).join('\n');
-  console.log(output);
-
-
-
+  })
+  .join("\n");
+console.log(output);
